a74d2a848568b697b4ee19e8547cbab3d74b5515
Luisfayre <luisfayre@hotmail.com>
2020-03-24 14:06:56 -0700

    Schedule

--- app/src/main/java/com/example/visitas/fragments/ScheduleFragment.java
+++ app/src/main/java/com/example/visitas/fragments/ScheduleFragment.java
@@ -24,11 +24,19 @@ public class ScheduleFragment extends Fragment {
 
     View theInflatedView;
 
-    ScheduleModel[] scheduleModels = {
-            new ScheduleModel("Bimbo","Visita a vimbo","Moviles"),
-            new ScheduleModel("Bimbo","Visita a vimbo","Moviles"),
-            new ScheduleModel("Bimbo","Visita a vimbo","Moviles"),
-            new ScheduleModel("Bimbo","Visita a vimbo","Moviles")
+    ScheduleModel[] schedule = {
+            new ScheduleModel("Bimbo","Visita a Bimbo","Moviles"),
+            new ScheduleModel("Bimbo 2","Visita a Bimbo","Moviles"),
+            new ScheduleModel("Bimbo 3","Visita a Bimbo","Moviles"),
+            new ScheduleModel("Bimbo 4","Visita a Bimbo","Moviles"),
+            new ScheduleModel("Bimbo 5","Visita a Bimbo","Moviles"),
+            new ScheduleModel("Bimbo 6","Visita a Bimbo","Moviles"),
+            new ScheduleModel("Bimbo 7","Visita a Bimbo","Moviles"),
+            new ScheduleModel("Bimbo 8","Visita a Bimbo","Moviles"),
+            new ScheduleModel("Bimbo 9","Visita a Bimbo","Moviles"),
+            new ScheduleModel("Bimbo 10","Visita a Bimbo","Moviles")
+
+
 
     };
 
@@ -41,15 +49,15 @@ public class ScheduleFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        theInflatedView = inflater.inflate(R.layout.fragment_schedule, container, false);
+        theInflatedView = inflater.inflate(R.layout.fragment_group, container, false);
 
 
-        listView = theInflatedView.findViewById(R.id.listViewSchedule);
+        listView = theInflatedView.findViewById(R.id.listViewGroups);
 
-//        adapter = new ScheduleModel(theInflatedView.getContext(),R.layout.item_list_schedule, scheduleModels);
+        adapter = new ScheduleAdapter(theInflatedView.getContext(),R.layout.item_list_schedule, schedule);
 
         // Inflate the layout for this fragment
-//        listView.setAdapter(adapter);
+        listView.setAdapter(adapter);
 
         return theInflatedView;
     }
