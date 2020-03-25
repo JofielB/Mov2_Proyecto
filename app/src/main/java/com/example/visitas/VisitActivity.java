379b0569522646088333ad038d080111ad9dcbfe
Jofiel Batista Farias <jofielbatista@hotmail.com>
2020-03-25 14:37:16 -0700

    VisitsActivity added and animation from the Schedule fragment to the VisitsActivity

---
+++ app/src/main/java/com/example/visitas/VisitActivity.java
@@ -0,0 +1,42 @@
+package com.example.visitas;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VisitActivity extends AppCompatActivity {
+    ListView listView;
+    List<String> visitData;
+
+    private ActionBar toolBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visit);
+
+        toolBar = getSupportActionBar();
+        toolBar.setTitle("Visit");
+
+        visitData = new ArrayList<String>();
+        visitData.add("Empresa: Bimbo");
+        visitData.add("Profesor: Pepe");
+        visitData.add("Grupo: ADSE");
+        visitData.add("Fecha: 24/09/2020");
+        visitData.add("Hora: 10:00am");
+
+
+        listView = findViewById(R.id.visit_data_list);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, visitData);
+
+        // Inflate the layout for this fragment
+        listView.setAdapter(adapter);
+    }
+}
--- app/src/main/java/com/example/visitas/fragments/ScheduleFragment.java
+++ app/src/main/java/com/example/visitas/fragments/ScheduleFragment.java
@@ -1,14 +1,21 @@
 package com.example.visitas.fragments;
 
+import android.app.Activity;
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
 
+import androidx.core.view.ViewCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.visitas.R;
+import com.example.visitas.VisitActivity;
 import com.example.visitas.adapters.ScheduleAdapter;
 import com.example.visitas.models.ScheduleModel;
 
@@ -40,6 +47,7 @@ public class ScheduleFragment extends Fragment {
 
     };
 
+    ImageView visitImage;
 
     public ScheduleFragment() {
         // Required empty public constructor
@@ -56,8 +64,22 @@ public class ScheduleFragment extends Fragment {
 
         adapter = new ScheduleAdapter(theInflatedView.getContext(),R.layout.item_list_schedule, schedule);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                visitImage = view.findViewById(R.id.imageView2);
+                Intent intent = new Intent(theInflatedView.getContext(), VisitActivity.class);
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation((Activity) adapter.getContext(),
+                        visitImage,
+                        ViewCompat.getTransitionName(visitImage));
+                startActivity(intent,options.toBundle());
+            }
+        });
+
         // Inflate the layout for this fragment
         listView.setAdapter(adapter);
+
+
 
         return theInflatedView;
     }
